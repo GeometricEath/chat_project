b2074abca5905a877af1f105f5780cfeca77f93d
Geometr <geo.fsv.p@gmail.com>
2018-05-20 23:05:25 +0300

    + mock data

---
+++ src/mockdata/data.js
@@ -0,0 +1,27 @@
+export const mockdata = {
+    message: {
+        user:{
+            name: 'Генри',
+            photo: './src/img/people.png'
+        },
+        text: 'Первое сообщение!',
+        time: new Date().toLocaleTimeString(),
+    },
+    message1: {
+        user:{
+            name: 'Джейн',
+            photo: './src/img/people.png'
+        },
+        text: 'Второе сообщение',
+        time: new Date().toLocaleTimeString(),
+    },
+    message2: {
+        user:{
+            name: 'Генри',
+            photo: './src/img/people.png'
+        },
+        text: 'И еще одно',
+        time: new Date().toLocaleTimeString(),
+    }
+}
+
