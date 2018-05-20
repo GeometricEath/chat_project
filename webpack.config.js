efd55e9783ca6b6b812f7f363084d34b6ddc09e0
Geometr <geo.fsv.p@gmail.com>
2018-05-21 02:46:51 +0300

    + сборка стилей в webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: './index.js',
@@ -7,17 +8,25 @@ module.exports = {
     filename: './bundle.js'
   },
   mode: 'development',
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.css$/,
         use: [
           { loader: 'style-loader' },
-          {
-            loader: 'css-loader',
-          }
-          // MiniCssExtractPlugin.loader,
-          // "css-loader"
+          // {
+          //   loader: 'css-loader',
+          // }
+          MiniCssExtractPlugin.loader,
+          {loader:"css-loader"},
         ]
       }
     ]
